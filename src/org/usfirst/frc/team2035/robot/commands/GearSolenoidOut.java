f3a4f39759dd27f855fdf18278596a055d049f04
jrauh1 <jrauh@cusd.me>
2017-02-20 12:50:09 -0800

    New Gear System Code with one double Solenoid.

---
+++ src/org/usfirst/frc/team2035/robot/commands/GearSolenoidOut.java
@@ -0,0 +1,58 @@
+/*----------------------------------------------------------------------------*/
+
+/* Copyright (c) 2016 FIRST Team 2035. All Rights Reserved.                   */
+/* Open Source Software - may be modified and shared by FRC teams.            */
+/*----------------------------------------------------------------------------*/
+package org.usfirst.frc.team2035.robot.commands;
+
+import org.usfirst.frc.team2035.robot.subsystems.DriveTrain;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+import org.usfirst.frc.team2035.robot.OI;
+import org.usfirst.frc.team2035.robot.Robot;
+
+/**
+ *
+ * @author Team 2035
+ */
+public class GearSolenoidOut extends Command {
+
+
+    private final GearSystem gearSystem;
+    public static OI oi; 
+
+    public GearSolenoidOut() {
+    	super("GearSystem");
+    	train = Robot.getGearSystem();
+    	requires(gearSystem);
+    
+    }
+    
+
+// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+		oi = new OI();
+	}
+    protected void execute() {
+    
+    	gearSystem.setGearSolenoidForward();
+    }
+    
+    protected boolean isFinished() {
+    	return false;
+    }
+ // Called once after isFinished returns true
+  	@Override
+  	protected void end() {
+  		gearSystem.GearSolenoidStop();
+  	}
+     
+  // Called when another command which requires one or more of the same
+  	// subsystems is scheduled to run
+  	@Override
+  	protected void interrupted() {
+  	}
+  }
+}
---
+++ src/org/usfirst/frc/team2035/robot/subsystems/GearSystem.java
@@ -0,0 +1,54 @@
+import org.usfirst.frc.team2035.robot.subsystems.Override;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.VictorSP;
+
+// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+	
+public class GearSystem extends Subsystem {
+
+
+	private DoubleSolenoid doubSol;
+		
+
+	public GearSystem()
+	{
+		super("GearSystem");
+		/*I am assuming two motors for the elevator. one for the front. One for the back.*/
+	
+
+		
+		
+		//Declare PWMS
+		doubSol = new DoubleSolenoid(0, 0, 0);
+		
+	}
+
+	
+	public void gearSolenoidSetForward() {
+		
+		doubSol.set(DoubleSolenoid.Value.kForward);
+		System.out.println("Gear solenoid out");
+	}
+
+	public void gearSolenoidSetReverse() {
+		doubSol.set(DoubleSolenoid.Value.kReverse);
+		System.out.println("Gear solenoid in");
+	}
+	
+	public void GearSolenoidStop(){
+		
+		doubSol.set(DoubleSolenoid.Value.kOff);
+		System.out.println("Gear solenoid stopping");
+	}
+	
+	
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
