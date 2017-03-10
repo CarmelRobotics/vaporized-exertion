cdb480728243c1bf6f3dcdc3f89a8406d4f64a3c
Abby Lambert <absterninja@gmail.com>
2017-03-09 16:34:41 -0800

    Added licensing - TODO: format and document code

--- src/org/usfirst/frc/team2035/robot/commands/ToggleVision.java
+++ src/org/usfirst/frc/team2035/robot/commands/ToggleVision.java
@@ -1,3 +1,9 @@
+/*----------------------------------------------------------------------------*/
+/*  (c) 2017 FIRST Team 2035. All Rights Reserved.                  		  */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
 package org.usfirst.frc.team2035.robot.commands;
 
--- src/org/usfirst/frc/team2035/robot/subsystems/NewElevator.java
+++ src/org/usfirst/frc/team2035/robot/subsystems/NewElevator.java
@@ -1,3 +1,10 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017 FIRST Team 2035. All Rights Reserved.                   */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 package org.usfirst.frc.team2035.robot.subsystems;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
